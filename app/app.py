a8feade597c97bcc4ec7eeb1532e6580c0ad0bd0
Ankita Puri <pr23.ankita@gmail.com>
2021-07-07 00:18:20 +1000

    fix flask app

--- app/app.py
+++ app/app.py
@@ -1,8 +1,8 @@
 
-from flask import Flask
+from flask import Flask, render_template
   
 app = Flask(__name__)
   
 @app.route("/")
 def home_view():
-        return "<h1>Welcome to final project</h1>"
+        return render_template("index.html")
